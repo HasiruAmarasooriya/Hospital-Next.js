6603b38c9eabed1b1023764d1e214586aa67506d
IT20113490_H.D.Amarasooriya <it20113490@my.sliit.lk>
2025-03-11 16:51:32 +0530

    new

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,148 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  DollarSign,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  Settings2,
+  SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Kumudu Hospital",
+      logo: GalleryVerticalEnd,
+      plan: "Matale",
+    },
+    
+  ],
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: SquareTerminal,
+      isActive: true,
+      
+    },
+    {
+      title: "Cashier",
+      url: "#",
+      icon: DollarSign,
+      isActive: true,
+      items: [
+        {
+          title: "Cashier",
+          url: "#",
+        },
+        {
+          title: "Other Income/Expenses",
+          url: "#",
+        },
+        {
+          title: "Cashier Sessions",
+          url: "#",
+        },
+        {
+          title: "Reports",
+          url: "#",
+        },
+        {
+          title: "Claim Bill",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Operations",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Channeling",
+          url: "#",
+        },
+        {
+          title: "OPD",
+          url: "#",
+        },
+        {
+          title: "X-Ray",
+          url: "#",
+        },
+        {
+          title: "Admission",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Master Data",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Channeling",
+          url: "#",
+        },
+        {
+          title: "OPD",
+          url: "#",
+        },
+        {
+          title: "X-Ray",
+          url: "#",
+        },
+        {
+          title: "Admission",
+          url: "#",
+        },
+      ],
+    },
+   
+  ]
+  
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
